f4f2f1eb306a7edf0ccde06d3fe4dfe767cf4894
Ronaiza Cardoso <ronaizacard@gmail.com>
2017-03-10 00:15:58 -0300

    Fecthing data

--- src/App.js
+++ src/App.js
@@ -16,8 +16,11 @@ class App extends Component {
     console.log(this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?'
     const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`
-
-    console.log(FETCH_URL);
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
   }
 
   render() {
