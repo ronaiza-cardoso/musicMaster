1dc060a2700b4de2b358341a43dbfc378e3d6c5c
Ronaiza Cardoso <ronaizacard@gmail.com>
2017-03-10 02:27:17 -0300

    Adding preview song

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,16 +2,60 @@ import React, { Component } from 'react'
 import './App.css'
 
 class Gallery extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playingUrl: '',
+      audio: null,
+      playing: false
+    }
+  }
+
+  playPreview(previewUrl) {
+    const audio = new Audio(previewUrl)
+    if (!this.state.playing) {
+      audio.play()
+      this.setState({
+        playing: true,
+        playingUrl: previewUrl,
+        audio
+      })
+    } else {
+      if (this.state.playingUrl === previewUrl) {
+        this.state.audio.pause()
+        this.setState({
+          playing: false
+        })
+      } else {
+        this.state.audio.pause()
+        audio.play()
+        this.setState({
+          playing: true,
+          playingUrl: previewUrl,
+          audio
+        })
+      }
+    }
+  }
+
   render() {
-    console.log('props', this.props);
     const tracks = this.props.tracks
     return (
       <div>
         {tracks.map((track, key) => {
           const trackImg = track.album.images[0].url
           return (
-            <div key={key} className="track">
+            <div key={key} className="track" onClick={() => this.playPreview(track.preview_url)}>
               <img src={trackImg} className="track-img" alt="track"/>
+              <div className="track-play">
+                <div className="track-play-inner">
+                  {
+                    this.state.playingUrl === track.preview_url
+                    ? <span>| |</span>
+                    : <span>&#9654;</span>
+                  }
+                </div>
+              </div>
               <p className="track-text">{track.name}</p>
             </div>
           )
